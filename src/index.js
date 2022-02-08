ef06fd3256ddae474105dde797b911a4dd91b774
Natalia <natellaozabota@gmail.com>
2022-02-08 18:29:10 +0300

    add github action to codesandbox

--- src/index.js
+++ src/index.js
@@ -11,6 +11,15 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+const myName = 'Alexander';
+
+ReactDOM.render(
+ <React.StrictMode>
+   <App name={myName} />
+ </React.StrictMode>,
+ document.getElementById("root")
+);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
